a60e7b1f929f5f51194fa2fee5e0a13f0c211a39
TheKevinNeville <kevin@neville.se>
2015-09-03 22:09:56 +0200

    Funjar?

--- MinTestKod.R
+++ MinTestKod.R
@@ -1,4 +1,13 @@
 fungerar <- c(1:100, c(-1, -1,-2))
-fungerar
+# Här tog jag bort
 
 # Här skriver jag lite till
+
+# Sen lägger jag till
+blirDetTacosImorgon <- function(){
+  if (rnorm(1,sd=1, mean=0) > 0){
+    print("Det blir tacos imorgon!")
+  } else print("Nej fan det blir det inte!")
+}
+blirDetTacosImorgon()
+
