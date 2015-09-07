0390753bdc02b6ac25a966a9a090061a2cee122c
sternis <gustav.sternelov@hotmail.com>
2015-09-07 14:56:08 +0200

    testing tests

--- KevGus/tests/testthat/test_funcs.R
+++ KevGus/tests/testthat/test_funcs.R
@@ -1,7 +1,10 @@
+
+test_that("output is correct",{
+  expect_equal(euclidian(100,1000), 100)
+  expect_equal(euclidian(123612, 13892347912), 4)
+})
+
 test_that("e and t are numeric values",{
   expect_equal(is.numeric(e), TRUE)
   expect_equal(is.numeric(t), TRUE)
 })
-
-
-expect_output(euclidian(100,1000), 100)
