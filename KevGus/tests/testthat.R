c1cdd15de23273cde95d96110aebc39a4ef939f2
sternis <gustav.sternelov@hotmail.com>
2015-09-07 14:38:19 +0200

    removing things

--- KevGus/tests/testthat.R
+++ KevGus/tests/testthat.R
@@ -1,9 +1,4 @@
 library(testthat)
 library(KevGus)
 
-test_that("e and t are numeric values",{
-  expect_equal(is.numeric(e), TRUE)
-  expect_equal(is.numeric(t), TRUE)
-  })
-
 test_check("KevGus")
