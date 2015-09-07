21a07aab701a7930dd1fd72adea4354fb8606cf0
sternis <gustav.sternelov@hotmail.com>
2015-09-07 14:30:58 +0200

    test-test

--- KevGus/tests/testthat.R
+++ KevGus/tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(KevGus)
 
+test_that("e and t are numeric values",{
+  expect_equal(is.numeric(e), TRUE)
+  expect_equal(is.numeric(t), TRUE)
+  })
+
 test_check("KevGus")
