82414d1c5d08cafc95add736167eaa7d3cb25ab5
sternis <gustav.sternelov@hotmail.com>
2015-09-08 09:55:58 +0200

    changes to test

--- KevGus/tests/testthat/test_funcs.R
+++ KevGus/tests/testthat/test_funcs.R
@@ -4,7 +4,3 @@ test_that("output is correct",{
   expect_equal(euclidian(123612, 13892347912), 4)
 })
 
-test_that("e and t are numeric values",{
-  expect_equal(is.numeric(e), TRUE)
-  expect_equal(is.numeric(t), TRUE)
-})
